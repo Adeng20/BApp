a02051f3f717abb394123ecdeceb6740d8984629
Adeng20 <104329126+Adeng20@users.noreply.github.com>
2025-08-06 15:52:32 +0300

    Update app.py
    app.server

--- app.py
+++ app.py
@@ -49,6 +49,8 @@ threading.Thread(target=fetch_data, daemon=True).start()
 app = dash.Dash(__name__)
 app.title = "BeatBnk Analytics Dashboard"
 
+server = app.server
+
 app.layout = html.Div([
     html.H1("BeatBnk Data Dashboard", style={"textAlign": "center"}),
     dcc.Tabs([
@@ -124,3 +126,4 @@ def update_payments_tab(n):
 
 if __name__ == '__main__':
     app.run(debug=True, port=8050)
+
